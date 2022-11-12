04b9e782b1fdc6c63449bfb1cd3e46b96a7fb468
savakarrohan <rohansavakar@gmail.com>
2022-11-12 22:40:56 +0530

    functional view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 # Create your views here.
 def post_list(request):
     """ Post list view"""
@@ -11,6 +11,9 @@ def post_list(request):
     page_number = request.GET.get('page',1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer
+        posts = paginator.page(1)
     except EmptyPage:
         # IF page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
