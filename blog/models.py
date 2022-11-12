e30df0d0c7abda2c24818425d2232d3d918db3ca
savakarrohan <rohansavakar@gmail.com>
2022-11-12 14:03:03 +0530

    model for blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """ Post object """
+    title = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=240)
+    body = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.title
+    
