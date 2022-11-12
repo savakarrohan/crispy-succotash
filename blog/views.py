6d0c7ed548c4367d855ccd2778cc6512213087c8
savakarrohan <rohansavakar@gmail.com>
2022-11-12 22:21:19 +0530

    exception for bigger interger

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 # Create your views here.
 def post_list(request):
     """ Post list view"""
@@ -9,7 +9,11 @@ def post_list(request):
     # Paginator with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page',1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # IF page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,'blog/post/list.html',{'posts':posts})
 def post_detail(request, year, month, day, post):
     """Detail view for each post"""
