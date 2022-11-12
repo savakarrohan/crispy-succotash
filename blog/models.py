36fe21ae9816544e29024b124c81baa7edc7bb2d
savakarrohan <rohansavakar@gmail.com>
2022-11-12 18:43:21 +0530

    absolute url for models

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -35,4 +35,5 @@ class Post(models.Model):
     
     def __str__(self) -> str:
         return self.title
-    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.id] )
