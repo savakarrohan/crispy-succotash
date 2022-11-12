6ff50349aa133fbc634497c53453d269ea91ff00
savakarrohan <rohansavakar@gmail.com>
2022-11-12 18:58:36 +0530

    slug and SEO friendly links

--- blog/models.py
+++ blog/models.py
@@ -36,4 +36,4 @@ class Post(models.Model):
     def __str__(self) -> str:
         return self.title
     def get_absolute_url(self):
-        return reverse('blog:post_detail',args=[self.id] )
+        return reverse('blog:post_detail',args=[self.publish.year, self.publish.month, self.publish.day, self.slug] )
--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ def post_list(request):
     """ Post list view"""
     posts = Post.published.all()
     return render(request,'blog/post/list.html',{'posts':posts})
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     """Detail view for each post"""
-    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     
     return render(request,'blog/post/detail.html',{
         'post':post
