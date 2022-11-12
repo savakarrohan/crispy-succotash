5a22735912afce510418cf19f4505c55a5ceed9e
savakarrohan <rohansavakar@gmail.com>
2022-11-12 15:51:00 +0530

    shortcut

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 # Create your views here.
@@ -8,10 +8,7 @@ def post_list(request):
     return render(request,'blog/post/list.html',{'posts':posts})
 def post_detail(request, id):
     """Detail view for each post"""
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
+    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
     
     return render(request,'blog/post/detail.html',{
         'post':post
