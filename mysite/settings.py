88d52a2648b91ce5b786d013bebb651bb14a180d
savakarrohan <rohansavakar@gmail.com>
2022-11-13 20:34:34 +0530

    Email server configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rohansavakar@gmail.com'
+EMAIL_HOST_PASSWORD = 'ngkirniksxvsciqp'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
