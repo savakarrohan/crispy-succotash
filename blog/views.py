468f3d585157c0eaf568a4576c398b09975bb3ab
savakarrohan <rohansavakar@gmail.com>
2022-11-13 21:03:02 +0530

    removed functional list view

--- blog/views.py
+++ blog/views.py
@@ -5,22 +5,6 @@ from django.views.generic import ListView
 from .forms import EmailPostForm
 from django.core.mail import send_mail
 # Create your views here.
-# def post_list(request):
-#     """ Post list view"""
-#     post_list = Post.published.all()
-#     # Paginator with 3 posts per page
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page',1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         # If page is not an integer
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # IF page_number is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,'blog/post/list.html',{'posts':posts})
-
 def post_detail(request, year, month, day, post):
     """Detail view for each post"""
     post = get_object_or_404(Post,status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
