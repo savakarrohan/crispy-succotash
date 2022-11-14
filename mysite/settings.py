8add50d0bf11944a5463a58631855f5606eb3692
savakarrohan <rohansavakar@gmail.com>
2022-11-15 00:28:45 +0530

    forgot commas

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Custom apps
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
     # Third party apps
     'taggit'
 ]
