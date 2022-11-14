9f235f3f35003298848014e335435c1d0d32fea7
savakarrohan <rohansavakar@gmail.com>
2022-11-15 00:27:17 +0530

    manager added to tag posts

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -22,6 +23,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     # Choices for publishing
     status = models.CharField(max_length=2, choices=Status.choices, default= Status.DRAFT)
+    # Taggit manager
+    tags = TaggableManager()
     
     objects = models.Manager()
     published = PublishedManager()
