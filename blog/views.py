4a8a9b4c1d2ada8ec8ec315e8e4db5704acdd425
savakarrohan <rohansavakar@gmail.com>
2022-11-15 18:11:29 +0530

    added tags to the page layout

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
+
 # Create your views here.
 def post_detail(request, year, month, day, post):
     """Detail view for each post"""
@@ -15,10 +17,16 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for the users to comment
     form = CommentForm
+    
+    # Implementing the tags information
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','publish')[:4]
     return render(request,'blog/post/detail.html',{
         'post':post,
         'comments':comments,
         'form':form,
+        'similar_posts':similar_posts
     })
     
     # Post_List view shouldn't have been deleted
