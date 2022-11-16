28b6a950755e9cbb68cc023475af8df53d12269f
savakarrohan <rohansavakar@gmail.com>
2022-11-16 22:53:27 +0530

    view for search

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,10 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from django.contrib.postgres.search import SearchVector
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Comment, Post
 
 
@@ -136,4 +137,24 @@ def post_comment(request, post_id):
             "form": form,
             "comment": comment,
         },
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
