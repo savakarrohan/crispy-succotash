51b1388d57c51aa71f73eb45149e1f58e3637a3d
savakarrohan <rohansavakar@gmail.com>
2022-11-16 17:36:25 +0530

    needed to use items function

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -6,7 +6,7 @@ class PostSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.9
 
-    def item(self):
+    def items(self):
         return Post.published.all()
 
     def lastmod(self, obj):
