840fa37ecfe3e4dfdc95f239163dd8ce746a7f6c
savakarrohan <rohansavakar@gmail.com>
2022-11-16 22:29:26 +0530

    migrated to the postgres database

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     # Sitemap framework
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    # Database apps
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -84,8 +86,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": "timeshift",
     }
 }
 
