7301a05492cd40f2f4bcd77b228a9d8d6c825d54
savakarrohan <rohansavakar@gmail.com>
2022-11-16 23:54:12 +0530

    Corrected with ranking

--- blog/urls.py
+++ blog/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
     path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
     path("feeds/", LatestPostFeed(), name="post_feed"),
+    path("search/", views.post_search, name="post_search"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
 
 from .forms import CommentForm, EmailPostForm, SearchForm
@@ -149,9 +149,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body"),
-            ).filter(search=query)
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query)
+                )
+                .filter(search=query)
+                .order_by("-rank")
+            )
 
     return render(
         request,
