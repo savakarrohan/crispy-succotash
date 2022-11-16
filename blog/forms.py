ff05d62fd235ab1d35f0135cf3fbbce0d97a875d
savakarrohan <rohansavakar@gmail.com>
2022-11-16 22:41:38 +0530

    search query

--- blog/forms.py
+++ blog/forms.py
@@ -17,3 +17,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["name", "email", "body"]
+
+
+class SearchForm(forms.Form):
+    """
+    Search form on the main page
+    """
+
+    query = forms.CharField()
