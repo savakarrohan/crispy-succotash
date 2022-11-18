5f8281da6c0098156df887e9c3dc7b6aa2cbe39b
savakarrohan <rohansavakar@gmail.com>
2022-11-18 17:01:27 +0530

    form and class based login view

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    # Simple function based view
+    # path("login/", views.user_login, name="login"),
+    # New class based view
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
