1bafc3fb564c454ac6c64f109c259521760cc50d
savakarrohan <rohansavakar@gmail.com>
2022-11-18 17:25:03 +0530

    basehtml modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,3 +143,8 @@ EMAIL_HOST_USER = "rohansavakar@gmail.com"
 EMAIL_HOST_PASSWORD = "ngkirniksxvsciqp"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# Login views and e.t.c
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
