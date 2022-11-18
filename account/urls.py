b602eaccbc392a069d69243885c48cf1bfaecfe8
savakarrohan <rohansavakar@gmail.com>
2022-11-18 18:02:14 +0530

    password change links

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+from tkinter import N
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -9,4 +10,15 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", views.dashboard, name="dashboard"),
+    # Change password urls
+    path(
+        "password-change",
+        auth_views.PasswordChangeView.as_view(),
+        name="password-change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
