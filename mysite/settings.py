ea8e471527aef10cbcb5a74541bc57b128e64e4b
savakarrohan <rohansavakar@gmail.com>
2022-11-19 22:01:11 +0530

    profile and social auth user model joined

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,3 +171,18 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
     "988569714758-jd0m25v868njhil95v34ml0v9aj4ud6k.apps.googleusercontent.com"
 )
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
+
+
+# Social Auth pipeline to automatically create a Profile
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "account.authentication.create_profile",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+]
