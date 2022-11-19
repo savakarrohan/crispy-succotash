129fd8336a5bfea46eb269fbf09ba42e9b65d294
savakarrohan <rohansavakar@gmail.com>
2022-11-19 11:49:05 +0530

    syntax errors

--- account/views.py
+++ account/views.py
@@ -67,8 +67,12 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-    else
-    user_form = UserEditForm(instance=request.user)
-    profile_form = ProfileEditForm(instance=request.user.profile)
-    
-    return render(request, 'account/edit.html',{'user_form':user_form,'profile_form':profile_form})
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
