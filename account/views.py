a2140cfc85f39f4dc65db4aa129193c7a841753e
savakarrohan <rohansavakar@gmail.com>
2022-11-19 12:19:08 +0530

    user messages when saving editing form

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 # Create your views here.
 def user_login(request):
@@ -67,6 +68,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile saved successfully")
+        else:
+            messages.error(request, "error updating profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
