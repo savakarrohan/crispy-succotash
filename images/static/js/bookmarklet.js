9053b75da842bc5575da0e805d3e84d30d5b9825
savakarrohan <rohansavakar@gmail.com>
2022-11-21 09:57:33 +0530

    copied from repo

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -3,44 +3,66 @@ const styleUrl = siteUrl + "static/css/bookmarklet.css";
 const minWidth = 250;
 const minHeight = 250;
 
-var head = document.getElementsByTagName("head")[0];
-var link = document.createElement("link");
-link.rel = "stylesheet";
+// load CSS
+var head = document.getElementsByTagName("head")[0]; // Get HTML head element
+var link = document.createElement("link"); // Create new link Element
+link.rel = "stylesheet"; // set the attributes for link element
 link.type = "text/css";
-link.href = styleUrl + "?=" + Math.floor(Math.random() * 9999999999999999);
-head.appendChild(link);
+link.href = styleUrl + "?r=" + Math.floor(Math.random() * 9999999999999999);
+head.appendChild(link); // Append link element to HTML head
 
-// Load the Html
+// load HTML
 var body = document.getElementsByTagName("body")[0];
-boxHtml =
-  '<div id="bookmarklet"> <a href="#" id="close">&times;</a> <h1>Select an image to bookmark:</h1> <div class="images"></div> </div>';
+boxHtml = `
+  <div id="bookmarklet">
+    <a href="#" id="close">&times;</a>
+    <h1>Select an image to bookmark:</h1>
+    <div class="images"></div>
+  </div>`;
 body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
   bookmarklet = document.getElementById("bookmarklet");
-  var imagesFound = bookmarklet.queryselector(".images");
+  var imagesFound = bookmarklet.querySelector(".images");
 
-  //   Clear images found
+  // clear images found
   imagesFound.innerHTML = "";
-  // Display bookmarklet
+  // display bookmarklet
   bookmarklet.style.display = "block";
 
-  // Close event
-  bookmarklet.queryselector("#close").addEventListener("click", function () {
+  // close event
+  bookmarklet.querySelector("#close").addEventListener("click", function () {
     bookmarklet.style.display = "none";
   });
 
-  //   find images in DOM with the minimum dimensions
+  // find images in the DOM with the minimum dimensions
   images = document.querySelectorAll(
-    'img[src$=".jpg"],img[src$=".jpeg"],img[src$=".png"]'
+    'img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]'
   );
   images.forEach((image) => {
-    if (image.naturalWidth >= minWidth && image.nautralHeight >= minHeight) {
+    if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
       var imageFound = document.createElement("img");
       imageFound.src = image.src;
       imagesFound.append(imageFound);
     }
   });
+
+  // select image event
+  imagesFound.querySelectorAll("img").forEach((image) => {
+    image.addEventListener("click", function (event) {
+      imageSelected = event.target;
+      bookmarklet.style.display = "none";
+      window.open(
+        siteUrl +
+          "images/create/?url=" +
+          encodeURIComponent(imageSelected.src) +
+          "&title=" +
+          encodeURIComponent(document.title),
+        "_blank"
+      );
+    });
+  });
 }
 
+// launch the bookmkarklet
 bookmarkletLaunch();
